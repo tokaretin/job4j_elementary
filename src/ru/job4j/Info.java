6c5749609e61b2f28faf9f0515b9ab7a4937aab5
tokaretin <tokaretin@gmail.com>
2020-10-10 00:06:21 +0300

    метод main с выводом сегодняшней даты

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("09.10.2020 ");
+        System.out.println("09.10.2020");
     }
 }
