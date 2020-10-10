2efd01dc80d7bc3d87c280ef5f6d705f27a91501
tokaretin <tokaretin@gmail.com>
2020-10-10 19:05:36 +0300

    Задание 0. Что такое переменная. [#382922]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculate;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+// Давайте потренируемся.
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int dddddd = 100500;
+        System.out.println(dddddd);
+        long money = 900500;
+        System.out.println(money);
+        float wight = 1.05F;
+        System.out.println(wight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        char gender = 'Y';
+        System.out.println(gender);
+
+        // задание 0. Что такое переменная. [#382922]
+        int age = 18;
+        System.out.println(age);
+        float  lenght = 78.0332;
+        System.out.println(lenght);
+        boolean baby = true;
+        System.out.println(baby);
+    }
+}
