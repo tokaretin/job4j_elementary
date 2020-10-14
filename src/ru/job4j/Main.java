3373bf15ce8289a2721762171cc3105314a18db2
tokaretin <tokaretin@gmail.com>
2020-10-14 20:37:46 +0300

    9.2. Checkstyle [#382923]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        String hiworld = "Hello world!";
+        String hiworld = "Hello my world!";
         System.out.println(hiworld);
     }
 }
