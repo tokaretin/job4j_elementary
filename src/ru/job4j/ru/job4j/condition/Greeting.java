ceef77aa08b502f2abb169c97eed96207c658bf5
tokaretin <tokaretin@gmail.com>
2020-10-14 20:07:38 +0300

    9.1. String [#382918]

---
+++ src/ru/job4j/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String separate = " ";
+        String newbie = "But I'm a newbie";
+        System.out.println(idea + separate + newbie);
+        int year = 2020;
+        System.out.println(idea + separate + year);
+    }
+}
