47cea9e01ed757c383db07aecd5399440fc4b928
tokaretin <tokaretin@gmail.com>
2020-10-17 23:15:07 +0300

    0. Что такое тестирование [#382939]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,7 +2,9 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        String hiworld = "Hello my world!";
-        System.out.println(hiworld);
+        //String hiworld = "Hello my world!";
+        //System.out.println(hiworld);
+        double result = Math.sqrt(9);
+        System.out.println(result);
     }
 }
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,11 +10,27 @@ public class Converter {
         int rsl = value / 60;
         return rsl;
     }
-
+/*
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro");
         int dollar = Converter.rubleToDollar(120);
         System.out.println("120 rubles are " + dollar + " dollar");
+    }
+}
+ */
+    public static void main(String[] args) {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result: " + passed);
+
+        int in2 = 180; // Специально сднлал такое значение, чтобы увмдеть false
+        int expected2 = 2;
+        int out2 = Converter.rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("180 rubles are 2. Test result: " + passed2);
+
     }
 }
