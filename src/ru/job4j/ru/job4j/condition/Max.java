b626622f34fd601e38d92f70e3eb3e251a8b3a1f
tokaretin <tokaretin@gmail.com>
2020-11-05 20:43:19 +0300

    4.2. Тернарное сравнение [#382868]

--- src/ru/job4j/ru/job4j/condition/Max.java
+++ src/ru/job4j/ru/job4j/condition/Max.java
@@ -1,10 +1,22 @@
 package ru.job4j.ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        left = 1;
-        right = 2;
-        int result = left + right;
-        return result;
+    public static int max(int x1, int x2) {
+        int max = x1 >= x2 ? x1 : x2;
+
+        //ТИП_ПЕРЕМЕННОЙ - int
+        // ИМЯ_ПЕРЕМЕННОЙ - max
+        // =
+        // ЛОГИЧЕСКОЕ_ВЫРАЖЕНИЕ - x1 >= x2
+        // ?
+        // ЗНАЧЕНИЕ_1 - x1
+        // :
+        // ЗНАЧЕНИЕ_2; - x2
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int rsl = Max.max(5, 3);
+        System.out.println(rsl);
     }
 }
