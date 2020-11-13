2e39e3323d709bf5d9584f15bf231ed4ec0efa4d
tokaretin <tokaretin@gmail.com>
2020-11-13 21:38:44 +0300

    4.4. Вложенный оператор if else [#382869]

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class MultiMax {
+
+    public static int max(int first, int second, int third) {
+
+        int result = first;
+        if (first > second) {
+            if (first > third)
+                result = first;
+        } else if (second > third) {
+            result = second;
+        } else {
+            result = third;
+        }
+        return result;
+    }
+}
+
+// Второй вариант с &&
+
+//    public static int max(int first, int second, int third) {
+//
+//        int result = first;
+//        if (first > second && first > third) {
+//                result = first;
+//        } else if (second > third && second > first) {
+//            result = second;
+//        } else {
+//            result = third;
+//        }
+//        return result;
+//    }
+//}
---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        int result = MultiMax.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirstMax() {
+        int result = MultiMax.max(7, 4, 2);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        int result = MultiMax.max(1, 4, 12);
+        assertThat(result, is(12));
+    }
+}
