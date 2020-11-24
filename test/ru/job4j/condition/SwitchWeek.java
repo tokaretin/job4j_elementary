fb065d0edf0d3bb2f8f59e59271c492b5d89014c
tokaretin <tokaretin@gmail.com>
2020-11-24 19:08:54 +0300

    4.5. Отладка программы в IDEA [#382854]

---
+++ test/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,10 @@
+//package ru.job4j.condition;
+//
+//public class SwitchWeek {
+//    public String nameOfDay(int day) {
+//        String name;
+//        case 1:
+//        nameOfDay(1) = "Monday";
+//        break;
+//    }
+//}
