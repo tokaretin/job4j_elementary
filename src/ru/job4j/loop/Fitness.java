2262541fe20aa6bfaddcb86354950b2075788a39
tokaretin <tokaretin@gmail.com>
2020-12-10 19:16:29 +0300

    5.4. Протеиновая диета [#382964]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            month++;
+            ivan *= 3;
+            nik *= 2;
+            System.out.println(month);
+        }
+        if (ivan == nik) {
+            month = 1;
+        }
+        return month;
+    }
+}
+
+
