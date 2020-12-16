7833afe57620fcf14a02df9098435be4da52fd9a
tokaretin <tokaretin@gmail.com>
2020-12-16 20:00:05 +0300

    5.5. Простое число [#382967]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            } else {
+                return prime;
+            }
+        }
+        return prime;
+    }
+}
