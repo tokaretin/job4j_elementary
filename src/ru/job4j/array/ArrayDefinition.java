d6d7163d2b55ac1dd84d676de608c5c587b78cef
tokaretin <tokaretin@gmail.com>
2021-01-07 18:24:44 +0300

    6.0.1.1. Размер массива [#382900]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println(ages.length);
-        System.out.println(surnames.length);
-        System.out.println(prices.length);
+        System.out.println("Размер массива short[] ages равен: " + ages.length);
+        System.out.println("Размер массива String[] surnames равен: " + surnames.length);
+        System.out.println("Размер массива float[] prices равен: " + prices.length);
+        System.out.println();
+
+        String[] names = new String[4];
+        names[0] = "Simona";
+        names[1] = "Ilia";
+        names[2] = "Lev";
+        names[3] = "Anastasia";
+        System.out.println("Размер массива String[] names равен: " + names.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
