ab7b404cafa1e94ee6b3aec843d12130ad7e69f5
tokaretin <tokaretin@gmail.com>
2021-02-16 20:03:07 +0300

    6.5.3. Сортировка выборкой. [#382883]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -14,7 +14,7 @@ public class FindLoop {
 
     public static int indexOff(int[] data, int el, int start, int finish) {
         int rst = -1; // Если не элемента в мвссиве то возвращаем -1
-        for (int i = start; i < finish; i++) {
+        for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
                 break;
