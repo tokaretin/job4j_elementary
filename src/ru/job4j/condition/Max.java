379d53430ac53192ed7ab874686ed7ac6d68aa8c
tokaretin <tokaretin@gmail.com>
2021-07-19 13:24:56 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -11,14 +11,27 @@ public class Max {
         );
     }
 
+    public static int max(int x1, int x2, int x3, int x4) {
+        return max(x1, max(x2, x3, x4)
+        );
+    }
+
     public static void main(String[] args) {
         int rsl = Max.max(5, 3);
         System.out.println(rsl);
         int result = Max.max(9, max(5, 8));
         System.out.println(result);
-        int rsl2 = Max.max(10,  max(22, 15));
+        int rsl2 = Max.max(10, max(22, 15));
         System.out.println(rsl2);
-        int rsl3 = Max.max(10,  max(31, 33));
+        int rsl3 = Max.max(10, max(31, 33));
         System.out.println(rsl3);
+        int rsl4 = Max.max(20, max(13, 14, 15));
+        System.out.println(rsl4);
+        int rsl5 = Max.max(12, max(16, 14, 15));
+        System.out.println(rsl5);
+        int rsl6 = Max.max(12, max(16, 17, 15));
+        System.out.println(rsl6);
+        int rsl7 = Max.max(12, max(16, 17, 18));
+        System.out.println(rsl7);
     }
 }
--- test/ru/job4j/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/ru/job4j/condition/MaxTest.java
@@ -42,4 +42,28 @@ public class MaxTest {
         int result = Max.max(50, 52, 58);
         assertThat(result, is(58));
     }
+
+    @Test
+    public void whenMax1To2To3to4Then4() {
+        int result = Max.max(4, 2, 3, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMax10To11To12to13Then13() {
+        int result = Max.max(10, 13, 11, 12);
+        assertThat(result, is(13));
+    }
+
+    @Test
+    public void whenMax21To22To23to24Then24() {
+        int result = Max.max(21, 22, 24, 23);
+        assertThat(result, is(24));
+    }
+
+    @Test
+    public void whenMax31To32To33to34Then34() {
+        int result = Max.max(31, 32, 33, 34);
+        assertThat(result, is(34));
+    }
 }
